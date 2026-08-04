596f5b5af271d2335e2541deb531e33e7a1d14e2
saikia6901-hub <saikia6901@gmail.com>
2026-08-05 02:23:06 +0530

    Update certificate.js

--- js/certificate.js
+++ js/certificate.js
@@ -1,5 +1,45 @@
 function generateCertificate(){
 
-alert("Certificate Generated Successfully!");
+localStorage.setItem("name",
+document.getElementById("name").value);
 
-}1
+localStorage.setItem("father",
+document.getElementById("father").value);
+
+localStorage.setItem("village",
+document.getElementById("village").value);
+
+localStorage.setItem("ps",
+document.getElementById("ps").value);
+
+localStorage.setItem("district",
+document.getElementById("district").value);
+
+window.location.href="certificate.html";
+
+}
+
+if(document.getElementById("certificateNo")){
+
+document.getElementById("certificateNo").innerHTML=
+"GP-2026-"+Math.floor(Math.random()*100000);
+
+document.getElementById("date").innerHTML=
+new Date().toLocaleDateString();
+
+document.getElementById("name").innerHTML=
+localStorage.getItem("name");
+
+document.getElementById("father").innerHTML=
+localStorage.getItem("father");
+
+document.getElementById("village").innerHTML=
+localStorage.getItem("village");
+
+document.getElementById("ps").innerHTML=
+localStorage.getItem("ps");
+
+document.getElementById("district").innerHTML=
+localStorage.getItem("district");
+
+}
