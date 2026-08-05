83f559133914990527ee554b9e7f098496b06a15
saikia6901-hub <saikia6901@gmail.com>
2026-08-05 21:11:18 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -22,4 +22,12 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-export { db, auth, collection, addDoc, getDocs };
+export {
+  db,
+  auth,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  runTransaction
+};
