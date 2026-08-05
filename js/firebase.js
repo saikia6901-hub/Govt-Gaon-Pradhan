f85e5f6f6cfbf3509a54c43d74fb83d15c6d49ff
saikia6901-hub <saikia6901@gmail.com>
2026-08-05 19:25:39 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
 
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4yT9YQg8g35_1J_8ILgg5S2Ps2oi5_Cs",
@@ -13,18 +10,12 @@ const firebaseConfig = {
   projectId: "govt-gaon-pradhan",
   storageBucket: "govt-gaon-pradhan.firebasestorage.app",
   messagingSenderId: "695225920086",
-  appId: "1:695225920086:web:ecf455d3d58f94cee669f7",
-  measurementId: "G-R3043Y47D0"
+  appId: "1:695225920086:web:ecf455d3d58f94cee669f7"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-
-export { db, collection, addDoc, getDocs };
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
-
 const auth = getAuth(app);
 
-export { auth };
+export { db, auth, collection, addDoc, getDocs };
