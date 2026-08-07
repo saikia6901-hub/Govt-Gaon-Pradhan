ec49f842c954471faebbe22d51d817abada0c7ef
saikia6901-hub <saikia6901@gmail.com>
2026-08-07 16:54:03 +0530

    Create pdf.js

---
+++ js/pdf.js
@@ -0,0 +1,28 @@
+window.downloadPDF = async function () {
+
+    const { jsPDF } = window.jspdf;
+
+    const certificate = document.querySelector(".certificate");
+
+    const canvas = await html2canvas(certificate, {
+        scale: 2
+    });
+
+    const imgData = canvas.toDataURL("image/png");
+
+    const pdf = new jsPDF("p", "mm", "a4");
+
+    const pageWidth = 210;
+    const pageHeight = (canvas.height * pageWidth) / canvas.width;
+
+    pdf.addImage(imgData, "PNG", 0, 0, pageWidth, pageHeight);
+
+    const data = JSON.parse(localStorage.getItem("certificate"));
+
+    const fileName =
+        `${data.certificateNo}-${data.name}.pdf`
+            .replace(/\s+/g, "-");
+
+    pdf.save(fileName);
+
+};
